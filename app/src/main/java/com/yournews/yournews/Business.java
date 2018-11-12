0f1c6c98960fa8b50250fac26490cc4fb0b3f8fe
tonnymuchui <tonykanyingah@gmail.com>
2018-11-12 15:10:58 +0300

    fragment classes

---
+++ app/src/main/java/com/yournews/yournews/Business.java
@@ -0,0 +1,17 @@
+package com.yournews.yournews;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Business extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.business, container, false);
+    }
+}
