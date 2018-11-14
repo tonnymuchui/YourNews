79b0dd37c06d8c554969bee4bff0934b81d553f9
tonnymuchui <tonykanyingah@gmail.com>
2018-11-14 08:20:07 +0300

    request && response

--- app/src/main/java/com/yournews/yournews/Api/NewsService.java
+++ app/src/main/java/com/yournews/yournews/Api/NewsService.java
@@ -1,82 +1,19 @@
 package com.yournews.yournews.Api;
 import android.util.Log;
 
-import com.yournews.yournews.models.Article;
-
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
 import java.io.IOException;
-import java.util.ArrayList;
 
-import okhttp3.Call;
-import okhttp3.Callback;
-import okhttp3.HttpUrl;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
 
 public class NewsService {
-    public static void findNews( Callback callback) {
-        OkHttpClient client = new OkHttpClient();
-        HttpUrl.Builder urlBuilder = HttpUrl.parse(Api.BASE_URL).newBuilder();
-        urlBuilder.addQueryParameter("api_key", Api.NEWS_API_KEY);
-        String url = urlBuilder.build().toString();
-        Log.d("URL", url);
-
+    public String GET(OkHttpClient client, String url) throws IOException {
         Request request = new Request.Builder()
                 .url(url)
                 .build();
 
-        Call call = client.newCall(request);
-        call.enqueue(callback);
+        Response response = client.newCall(request).execute();
+        return response.body().string();
     }
-//    public static  ArrayList<Headline> processHeadline(Response response) throws JSONException,IOException {
-//        ArrayList<Headline> headlines = new ArrayList<>();
-//        Log.d("RESPONSE",response.body().toString());
-//        try {
-//            String jasonData = response.body().string();
-//            if (response.isSuccessful()){
-//                JSONObject newsJASON = new JSONObject(jasonData);
-//                JSONArray jsonArray = newsJASON.optJSONArray("headline");
-//                for (int i = 0;i < jsonArray.length();i++){
-//            JSONObject jsonArray1= jsonArray.getJSONObject(i);
-//            String status = jsonArray1.getString("status");
-//            Integer totalResult = jsonArray1.getInt("totalResult");
-//            ArrayList<Article> article = new ArrayList<>();
-//
-//            JSONArray articles = newsJASON.getJSONArray("articles");
-//            for (int y =0;y < articles.length();y++){
-//                JSONObject jsonArray2 = jsonArray.getJSONObject(y);
-//            String author = jsonArray2.getString("author");
-//             String title = jsonArray2.getString("title");
-//             String description = jsonArray2.getString("description");
-//              String url = jsonArray2.getString("url");
-//              String urlToImage = jsonArray2.getString("urlToImage");
-//              String publishedAt = jsonArray2.getString("publishedAt");
-//              ArrayList<Source> source= new ArrayList<>();
-//Article article1 = new Article(author,title,description,url,urlToImage,publishedAt,source);
-//              JSONArray sources = newsJASON.getJSONArray("sources");
-//              for (int x =0; x < sources.length();x++){
-//                  JSONObject jsonArray3 = jsonArray.getJSONObject(x);
-//                  Integer id = jsonArray3.getInt("id");
-//                  String name = jsonArray3.getString("name");
-//               Source source1 = new Source(id,name);
-//              }
-//            }
-//            Headline headline = new Headline(status,totalResult,article);
-//            headlines.add(headline);
-//            headlines.add()
-//
-//                }
-//            }
-//
-//        }catch (IOException e){
-//            e.printStackTrace();
-//        }
-//
-//        return null;
-//    }
 }
---
+++ app/src/main/java/com/yournews/yournews/TrendingAdapter.java
@@ -0,0 +1,67 @@
+package com.yournews.yournews;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+import com.yournews.yournews.models.News;
+
+import java.util.List;
+
+public class TrendingAdapter extends RecyclerView.Adapter<TrendingAdapter.myViewHolder> {
+
+    private List<News> results;
+    private Context context;
+
+    public TrendingAdapter( Context context,List<News> results) {
+        this.results = results;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public myViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View view= LayoutInflater.from(viewGroup.getContext())
+                .inflate(R.layout.list_item, viewGroup ,false);
+
+        return new myViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull myViewHolder viewHolder, int i) {
+        viewHolder.author.setText(results.get(1).getAuthor());
+        viewHolder.publishedAt.setText(results.get(i).getPublishedAt());
+        viewHolder.title.setText(results.get(i).getTitle());
+        viewHolder.desc.setText(results.get(i).getDescription());
+        viewHolder.source.setText((CharSequence) results.get(i).getSource());
+
+        Picasso.with(context).load("https://cdn.cnn.com/cnnnext/dam/assets/"+results.get(i)
+                .getUrlToImage()).into(viewHolder.imageView);
+    }
+
+    @Override
+    public int getItemCount() {
+        return results.size();
+    }
+    public class myViewHolder extends RecyclerView.ViewHolder {
+ImageView imageView;
+TextView author, publishedAt,title,desc,source,time;
+
+        public myViewHolder(@NonNull View itemView) {
+            super(itemView);
+    imageView = itemView.findViewById(R.id.img);
+    author = itemView.findViewById(R.id.author);
+    publishedAt = itemView.findViewById(R.id.publishedAt);
+    title = itemView.findViewById(R.id.title);
+    desc = itemView.findViewById(R.id.desc);
+    source = itemView.findViewById(R.id.source);
+    time = itemView.findViewById(R.id.time);
+        }
+    }
+}
